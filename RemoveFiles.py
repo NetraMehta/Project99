450c526383180e4709ddf99a88ff02167a2bd012
NetraMehta <78476060+NetraMehta@users.noreply.github.com>
2022-06-02 16:31:50 +0530

    Add files via upload

--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,37 +1,128 @@
-from importlib.metadata import files
-from logging import root
-import time
+# importing the required modules
 import os
 import shutil
+import time
 
-path = input('Enter folder path here: ')
-days = int(input('Enter number of days here: '))
+# main function
+def main():
 
-seconds = days*24*60*60
-print(seconds)
+	# initializing the count
+	deleted_folders_count = 0
+	deleted_files_count = 0
 
-isExist = os.path.exists(path)
-print(isExist)
+	# specify the path
+	path = input('Enter path here: ')
 
-if isExist:
-   for root, dirs, files in os.walk(path):
-       print(root)
+	# specify the days
+	days = 30
 
-       dirs[:] = [d for d in dirs if not d.startswith('.')]
+	# converting days to seconds
+	# time.time() returns current time in seconds
+	seconds = time.time() - (days * 24 * 60 * 60)
 
-       for dir in dirs:
-           print(os.path.join(root, dir))
+	# checking whether the file is present in path or not
+	if os.path.exists(path):
 
-       for file in files:
-           print(os.path.join(root, file))
-           fileOrigin = os.stat(path).st_ctime
-           print(fileOrigin)
+		# iterating over each and every folder and file in the path
+		for root_folder, folders, files in os.walk(path):
 
-           if fileOrigin > seconds:
-               os.remove(file)
+			# comparing the days
+			if seconds >= get_file_or_folder_age(root_folder):
 
-           else:
-               print('File not removed')
+				# removing the folder
+				remove_folder(root_folder)
+				deleted_folders_count += 1 # incrementing count
 
-else:
-    print('Path not found')
+				# breaking after removing the root_folder
+				break
+
+			else:
+
+				# checking folder from the root_folder
+				for folder in folders:
+
+					# folder path
+					folder_path = os.path.join(root_folder, folder)
+
+					# comparing with the days
+					if seconds >= get_file_or_folder_age(folder_path):
+
+						# invoking the remove_folder function
+						remove_folder(folder_path)
+						deleted_folders_count += 1 # incrementing count
+
+
+				# checking the current directory files
+				for file in files:
+
+					# file path
+					file_path = os.path.join(root_folder, file)
+
+					# comparing the days
+					if seconds >= get_file_or_folder_age(file_path):
+
+						# invoking the remove_file function
+						remove_file(file_path)
+						deleted_files_count += 1 # incrementing count
+
+		else:
+
+			# if the path is not a directory
+			# comparing with the days
+			if seconds >= get_file_or_folder_age(path):
+
+				# invoking the file
+				remove_file(path)
+				deleted_files_count += 1 # incrementing count
+
+	else:
+
+		# file/folder is not found
+		print(f'"{path}" is not found')
+		deleted_files_count += 1 # incrementing count
+
+	print(f"Total folders deleted: {deleted_folders_count}")
+	print(f"Total files deleted: {deleted_files_count}")
+
+
+def remove_folder(path):
+
+	# removing the folder
+	if not shutil.rmtree(path):
+
+		# success message
+		print(f"{path} is removed successfully")
+
+	else:
+
+		# failure message
+		print(f"Unable to delete the "+path)
+
+
+
+def remove_file(path):
+
+	# removing the file
+	if not os.remove(path):
+
+		# success message
+		print(f"{path} is removed successfully")
+
+	else:
+
+		# failure message
+		print("Unable to delete the "+path)
+
+
+def get_file_or_folder_age(path):
+
+	# getting ctime of the file/folder
+	# time will be in seconds
+	ctime = os.stat(path).st_ctime
+
+	# returning the time
+	return ctime
+
+
+if __name__ == '__main__':
+	main()
